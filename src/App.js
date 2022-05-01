3d6db652e72d66b0931f47bb7391795273450e17
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-05-01 14:17:26 -0700

    first commit and NavBar created

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Services from './pages/Services';
+import Blog from "./pages/Blog";
+import Gallary from "./pages/Gallary"
+
+function App() {
+  return (
+    <div>
+      <Router>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/gallary' element={<Gallary/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/services' element={<Services/>}/>
+      </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { AiOutlineHome } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  return (
+    <Container>
+      <NavContainer>
+        <Link to="/">
+          <LogoHold>
+            <HomeLogo>
+              <AiOutlineHome />
+            </HomeLogo>
+            <HomeText>Home</HomeText>
+          </LogoHold>
+        </Link>
+        <MobileIcon onClick={handleClick}>
+          {click ? <FaTimes /> : <GiHamburgerMenu />}
+        </MobileIcon>
+
+        <NavBarHolder onClick={handleClick} click={click}>
+          <NavLinks>
+            <Link to="/contact">Contact</Link>{" "}
+          </NavLinks>
+          <NavLinks>
+            <Link to="/product">Product</Link>
+          </NavLinks>
+          <NavLinks>
+            <Link to="/gallary">Gallary</Link>
+          </NavLinks>
+          <NavLinks>
+            <Link to="/services">Services</Link>
+          </NavLinks>
+          <NavLinks>
+            <Link to="/blog">Blog</Link>
+          </NavLinks>
+        </NavBarHolder>
+      </NavContainer>
+    </Container>
+  );
+};
+
+export default NavBar;
+
+const Container = styled.div`
+  display: flex;
+  height: 80px;
+  justify-content: center;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  box-shadow: 0px 4px 4px 4px rgba(225, 225, 225, 0.5);
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.7),
+    rgba(255, 255, 255, 0.3)
+  );
+`;
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  height: 80px;
+  z-index: 1;
+  background-color: black;
+  color: white;
+  align-items: center;
+`;
+const LogoHold = styled.div`
+  margin-left: 20px;
+  display: flex;
+  text-align: center;
+  align-items: center;
+`;
+const HomeLogo = styled.span`
+  font-size: 24px;
+`;
+const HomeText = styled.span``;
+
+const NavBarHolder = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  width: 500px;
+  justify-content: space-between;
+  padding-right: 30px;
+  font-weight: bold;
+
+  @media screen and (max-width: 850px) {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    padding-right: 0;
+    padding-left: 0;
+    top: 50px;
+    height: 30vh;
+    width: 100%;
+    justify-content: flex-start;
+    background: #101522;
+    left: ${({ click }) => (click ? 0 : "-100%")};
+    opacity: 1;
+    transition: all 0.5s ease;
+    gap: 10px;
+  }
+`;
+
+const NavLinks = styled.div``;
+
+const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 850px) {
+    color: white;
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+  }
+`;
